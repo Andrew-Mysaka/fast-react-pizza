d569fe986ca8c7a195575d628a24f160ac58e243
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-18 18:02:24 +0200

    Writing Data With React Router "Actions"

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./ui/Home.jsx";
 import Menu, {loader as menuLoader} from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
-import CreateOrder from "./features/order/CreateOrder.jsx";
+import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder.jsx";
 import Order, {loader as orderLoader}  from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 import Error from "./ui/Error.jsx";
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
             {path: '/', element: <Home/>},
             {path: '/menu', element: <Menu/>, loader: menuLoader, errorElement: <Error/>},
             {path: '/cart', element: <Cart/>},
-            {path: '/order/new', element: <CreateOrder/>},
+            {path: '/order/new', element: <CreateOrder/>, action: createOrderAction},
             {path: '/order/:orderId', element: <Order/>, loader: orderLoader, errorElement: <Error/>},
         ]
     },
