4623538769bd9218902e5175a1efe469ee4ef84c
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-18 21:28:57 +0200

    Tailwind: Working with colors

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import SearchOrder from "../features/order/SearchOrder.jsx";
 
 function Header() {
     return (
-        <header>
+        <header className={'bg-yellow-500'}>
             <Link to={'/'}>Fast React Pizza Co.</Link>
 
             <SearchOrder/>
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,10 +1,12 @@
 function Home() {
     return (
-        <div className={'text-xl text-yellow-500 font-semibold text-center'}>
+        <div className={'text-xl text-stone-700 font-semibold text-center'}>
             <h1>
                 The best pizza.
                 <br/>
-                Straight out of the oven, straight to you.
+                <span className={'text-yellow-500'}>
+                    Straight out of the oven, straight to you.
+                </span>
             </h1>
         </div>
     );
