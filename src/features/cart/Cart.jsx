038d7c95e6898e0593c21f2fbfb8afa1a4f63812
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-19 22:13:52 +0200

    Building the Cart Overview With Redux Selectors

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,35 +1,16 @@
 import LinkButton from "../../ui/LinkButton.jsx";
 import Button from "../../ui/Button.jsx";
 import CartItem from "./CartItem.jsx";
-import {useSelector} from "react-redux";
-
-const fakeCart = [
-    {
-        pizzaId: 12,
-        name: 'Mediterranean',
-        quantity: 2,
-        unitPrice: 16,
-        totalPrice: 32,
-    },
-    {
-        pizzaId: 6,
-        name: 'Vegetale',
-        quantity: 1,
-        unitPrice: 13,
-        totalPrice: 13,
-    },
-    {
-        pizzaId: 11,
-        name: 'Spinach and Mushroom',
-        quantity: 1,
-        unitPrice: 15,
-        totalPrice: 15,
-    },
-];
+import {useDispatch, useSelector} from "react-redux";
+import {clearCart, getCart} from "./cartSlice.js";
+import EmptyCart from "./EmptyCart.jsx";
 
 function Cart() {
-    const cart = fakeCart;
+    const cart = useSelector(getCart);
     const username = useSelector(state => state.user.username);
+    const dispatch = useDispatch();
+
+    if(!cart.length) return <EmptyCart/>
 
     return (
         <div className="px-4 py-3">
@@ -48,7 +29,7 @@ function Cart() {
                     Order pizzas
                 </Button>
 
-                <Button type="secondary">Clear cart</Button>
+                <Button type="secondary" onClick={() => dispatch(clearCart())}>Clear cart</Button>
             </div>
         </div>
     );
