0e85dcc84f4b667048d67939864109654615fdbd
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-19 20:41:01 +0200

    Modeling the "Cart" state

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
-import useReducer from "./features/user/userSlice.js";
+import userReducer from "./features/user/userSlice.js";
+import cartReducer from "./features/cart/cartSlice.js";
 
 const store = configureStore({
     reducer:{
-        user: useReducer,
+        user: userReducer,
+        cart: cartReducer
     }
 })
 
