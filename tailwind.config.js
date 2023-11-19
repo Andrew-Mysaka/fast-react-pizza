d4edd03bbbdc1ef07fd1b8a86fbd1549e2ba45c7
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-19 15:55:41 +0200

    Tailwind: configuring: custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        fontFamily: {
+            sans: 'Roboto Mono, monospace',
+        },
+        extend: {
+            height: {
+                screen: '100dvh'
+            }
+        },
+    },
+    plugins: [],
 }
 
