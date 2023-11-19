6c970d7572c2f6bbd95973fe7ae1811cc90f8b98
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-19 11:53:28 +0200

    Tailwind: styling form elements

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -18,6 +18,7 @@ function SearchOrder() {
                 placeholder={'Search order #'}
                 value={query}
                 onChange={e=>setQuery(e.target.value)}
+                className={'rounded-full bg-yellow-100 px-4 py-2 text-sm placeholder: text-stone-400 sm:focus:w-72 sm:w-64 transition-all duration-300 focus:outline-none focus:ring-opacity-50 focus:ring focus:ring-yellow-500 '}
             />
         </form>
     );
