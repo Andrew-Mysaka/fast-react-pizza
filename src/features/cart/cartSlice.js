bf8c6d4d9fc5959ba0be39774197cfebabb159ca
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-19 21:52:27 +0200

    Building the Cart Overview With Redux Selectors

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,16 +1,7 @@
 import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
-    // cart: [],
-    cart: [
-        {
-            pizzaId: 12,
-            name: 'Mediterranean',
-            quantity: 2,
-            unitPrice: 16,
-            totalPrice: 32,
-        }
-    ],
+    cart: [],
 }
 
 const cartSlice = createSlice({
@@ -44,3 +35,7 @@ const cartSlice = createSlice({
 export const {addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart} = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getTotalCartQuantity = state => state.cart.cart.reduce((sum,item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = state => state.cart.cart.reduce((sum,item) => sum + item.totalPrice, 0);
