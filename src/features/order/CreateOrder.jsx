2dc556451bd59a3c709a22bbd9c609a451fa8c97
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-19 17:55:34 +0200

    Tailwind: styling the order form

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -43,68 +43,60 @@ function CreateOrder() {
     const cart = fakeCart;
 
     return (
-        <div>
-            <h2>Ready to order? Let's go!</h2>
+        <div className="px-4 py-6">
+            <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
             {/*<Form method={'POST'} action={'/order/new'}>*/}
-            <Form method={'POST'}>
-                <div>
-                    <label>First Name</label>
-                    <input
-                        type="text"
-                        name="customer"
-                        required
-                        className={'input'}
-                    />
+            <Form method="POST">
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">First Name</label>
+                    <input className="input grow" type="text" name="customer" required />
                 </div>
 
-                <div>
-                    <label>Phone number</label>
-                    <div>
-                        <input
-                            type="tel"
-                            name="phone"
-                            required
-                            className={'input'}
-                        />
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">Phone number</label>
+                    <div className="grow">
+                        <input className="input w-full" type="tel" name="phone" required />
                         {formErrors?.phone && (
-                            <p>
+                            <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
                                 {formErrors.phone}
                             </p>
                         )}
                     </div>
                 </div>
 
-                <div>
-                    <label>Address</label>
-                    <div>
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">Address</label>
+                    <div className="grow">
                         <input
+                            className="input w-full"
                             type="text"
                             name="address"
                             required
-                            className={'input'}
                         />
                     </div>
                 </div>
 
-                <div>
+                <div className="mb-12 flex items-center gap-5">
                     <input
+                        className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
                         type="checkbox"
                         name="priority"
                         id="priority"
-                        className={'h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2'}
                         // value={withPriority}
                         // onChange={(e) => setWithPriority(e.target.checked)}
                     />
-                    <label htmlFor="priority">Want to yo give your order priority?</label>
+                    <label htmlFor="priority" className="font-medium">
+                        Want to yo give your order priority?
+                    </label>
                 </div>
 
-              <div>
-                  <input type={'hidden'} name={'cart'} value={JSON.stringify(cart)}/>
-                  <Button disabled={isSubmitting} type={'primary'}>
-                      {isSubmitting ? 'Placing order....' : 'Order now'}
-                  </Button>
-              </div>
+                <div>
+                    <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+                    <Button disabled={isSubmitting} type="primary">
+                        {isSubmitting ? 'Placing order....' : 'Order now'}
+                    </Button>
+                </div>
             </Form>
         </div>
     );
