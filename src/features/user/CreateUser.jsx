be532b70ab5b9c5df16068bcf4f3fa9084cd2d0c
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-19 20:13:16 +0200

    Reading and updating the user state

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,32 +1,42 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Button from "../../ui/Button.jsx";
+import {useDispatch} from "react-redux";
+import {updateName} from "./userSlice.js";
+import {useNavigate} from "react-router-dom";
 
 function CreateUser() {
-  const [username, setUsername] = useState('');
+    const [username, setUsername] = useState('');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
+    function handleSubmit(e) {
+        e.preventDefault();
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <p className={'mb-4 text-sm text-stone-600 md:text-base'}>👋 Welcome! Please start by telling us your name:</p>
+        if(!username) return;
+        dispatch(updateName(username));
+        navigate("/menu");
+    }
 
-      <input
-        type="text"
-        placeholder="Your full name"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className={'w-72 input mb-8'}
-      />
+    return (
+        <form onSubmit={handleSubmit}>
+            <p className={'mb-4 text-sm text-stone-600 md:text-base'}>👋 Welcome! Please start by telling us your
+                name:</p>
 
-      {username !== '' && (
-        <div>
-          <Button type={'primary'}>Start ordering</Button>
-        </div>
-      )}
-    </form>
-  );
+            <input
+                type="text"
+                placeholder="Your full name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className={'w-72 input mb-8'}
+            />
+
+            {username !== '' && (
+                <div>
+                    <Button type={'primary'}>Start ordering</Button>
+                </div>
+            )}
+        </form>
+    );
 }
 
 export default CreateUser;
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,11 @@
 import CreateUser from "../features/user/CreateUser.jsx";
+import {useState} from "react";
+import {useSelector} from "react-redux";
+import Button from "./Button.jsx";
 
 function Home() {
+    const username = useSelector(state => state.user.username);
+
     return (
         <div className={'my-10 text-center sm:my-16 px-4'}>
             <h1 className={'text-xl font-semibold mb-8 md:text-3xl'}>
@@ -11,7 +16,7 @@ function Home() {
                 </span>
             </h1>
 
-            <CreateUser/>
+            {username === "" ? <CreateUser/> : <Button to={'/menu'} type={'primary'}>Continue ordering, {username}</Button>}
         </div>
     );
 }
