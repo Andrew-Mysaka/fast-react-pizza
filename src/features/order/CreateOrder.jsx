2c3d7fb45b804332d97f219e8dc51f38d1df3615
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-21 20:44:38 +0200

    Redux Thunks With createAsyncThunk

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,11 +2,12 @@ import {useState} from "react";
 import {Form, redirect, useActionData, useNavigation} from "react-router-dom";
 import {createOrder} from "../../services/apiRestaurant.js";
 import Button from "../../ui/Button.jsx";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {clearCart, getCart, getTotalCartPrice} from "../cart/cartSlice.js";
 import EmptyCart from "../cart/EmptyCart.jsx";
 import store from "../../store.js";
 import {formatCurrency} from "../../utils/helpers.js";
+import {fetchAddress} from "../user/userSlice.js";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -45,28 +46,32 @@ function CreateOrder() {
     const username = useSelector(state => state.user.username);
 
     const formErrors = useActionData();
+    const dispatch = useDispatch();
+
     const cart = useSelector(getCart);
-    const totalCartPrice =  useSelector(getTotalCartPrice);
+    const totalCartPrice = useSelector(getTotalCartPrice);
     const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
     const totalPrice = totalCartPrice + priorityPrice
 
-    if(!cart.length) return  <EmptyCart/>
+    if (!cart.length) return <EmptyCart/>
 
     return (
         <div className="px-4 py-6">
             <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
+            <button onClick={() => dispatch(fetchAddress())}>Get position</button>
+
             {/*<Form method={'POST'} action={'/order/new'}>*/}
             <Form method="POST">
                 <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
                     <label className="sm:basis-40">First Name</label>
-                    <input className="input grow" type="text" name="customer" defaultValue={username} required />
+                    <input className="input grow" type="text" name="customer" defaultValue={username} required/>
                 </div>
 
                 <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
                     <label className="sm:basis-40">Phone number</label>
                     <div className="grow">
-                        <input className="input w-full" type="tel" name="phone" required />
+                        <input className="input w-full" type="tel" name="phone" required/>
                         {formErrors?.phone && (
                             <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
                                 {formErrors.phone}
@@ -102,7 +107,7 @@ function CreateOrder() {
                 </div>
 
                 <div>
-                    <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+                    <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
                     <Button disabled={isSubmitting} type="primary">
                         {isSubmitting ? 'Placing order....' : `Order now for ${formatCurrency(totalPrice)}`}
                     </Button>
@@ -112,29 +117,29 @@ function CreateOrder() {
     );
 }
 
-export async function action({request}){
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
+export async function action({request}) {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
 
-  const order = {
-    ...data,
-    cart: JSON.parse(data.cart),
-    priority: data.priority === "true"
-  }
+    const order = {
+        ...data,
+        cart: JSON.parse(data.cart),
+        priority: data.priority === "true"
+    }
 
-  const errors = {};
-  if (!isValidPhone(order.phone))
-    errors.phone = 'Please give us your correct phone number. We might need it to contact you.';
+    const errors = {};
+    if (!isValidPhone(order.phone))
+        errors.phone = 'Please give us your correct phone number. We might need it to contact you.';
 
-  if (Object.keys(errors).length > 0) return errors;
+    if (Object.keys(errors).length > 0) return errors;
 
-  // If everything is okay, create new order and redirect
-  const newOrder = await createOrder(order);
+    // If everything is okay, create new order and redirect
+    const newOrder = await createOrder(order);
 
-  // Do NOT overuse
-  store.dispatch(clearCart());
+    // Do NOT overuse
+    store.dispatch(clearCart());
 
-  return redirect(`/order/${newOrder.id}`);
+    return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
